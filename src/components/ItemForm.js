a9d368ea45a474dadcdd76b7e12401345b0f05e4
Parker Parham <parkerparham@gmail.com>
2022-09-07 17:23:25 -0400

    commit tree b79596c78e22e49216d023fa2886c7b0fb99945f

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onNewItem, itemName, onItemCat, itemCat, onItemFormSubmit }) {
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCat
+    }
+    onItemFormSubmit(newItem)
+    console.log(newItem)
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={itemName} onChange={onNewItem}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select value={itemCat} onChange={onItemCat} name="category">
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
